bf1974371b7b00b04c72aaea2454cc1ac28991ed
bringmethehorizon <teodor_me@yahoo.com>
2013-04-10 17:28:10 +0300

    input more matrixes

--- main.cpp
+++ main.cpp
@@ -38,12 +38,18 @@ void solver(int x, int y, wstring so_far, char depth, int thread_number)
 	{
 		results_mutex.lock();
 		results.push_back(so_far);
+		//results.insert(so_far);
 		results_mutex.unlock();
-	}//results.insert(so_far);
+	}
 	if(x-1>=0 && !used[thread_number][x-1][y]) solver(x-1,y,so_far,depth,thread_number);
 	if(x+1<ROWS && !used[thread_number][x+1][y]) solver(x+1,y,so_far,depth,thread_number);
 	if(y-1>=0 && !used[thread_number][x][y-1]) solver(x,y-1,so_far,depth,thread_number);
 	if(y+1<COLS && !used[thread_number][x][y+1]) solver(x,y+1,so_far,depth,thread_number);
+
+	if(x-1>=0 && y-1>=0 && !used[thread_number][x-1][y-1]) solver(x-1,y-1,so_far,depth,thread_number);
+	if(x+1<ROWS && y-1>=0 && !used[thread_number][x+1][y-1]) solver(x+1,y-1,so_far,depth,thread_number);
+	if(x-1>=0 && y+1<COLS && !used[thread_number][x-1][y+1]) solver(x-1,y+1,so_far,depth,thread_number);
+	if(x+1<ROWS && y+1<COLS && !used[thread_number][x+1][y+1]) solver(x+1,y+1,so_far,depth,thread_number);
 	used[thread_number][x][y]=false;
 }
 
@@ -89,44 +95,46 @@ struct comparator
 
 int main()
 {
-	//SetConsoleOutputCP(1251);
-	//SetConsoleCP(1251);
 	setlocale(LC_CTYPE, "");
 	_setmode(_fileno(stdout), _O_U8TEXT);
 	_setmode(_fileno(stdin), _O_U16TEXT);
 	thread th([]{dict.loadFile("bg1.dict");});	
-	//wchar_t c1;
 	memset(&used,0,ROWS*ROWS*ROWS);
-
-	wcout << L"Enter the matrix (4 lines with 4 letters NO SPACES):" << endl;
-	//wcin >> c1;
-	//wcout << c1 << endl;
-	//system("pause");
-	//return 0;
-	for(int i=0; i<ROWS; i++)
-	{
-		wcin >> matrix[i];
-		if(matrix[i].length()!=4)
-		{
-			wcout << L"\aYou made an error.\nEnter the matrix again from the beginning:" << endl;
-			//i=0;
-		}
-	}
-	wcout << L"\nSearching..." << endl;
+	bool more=true;
 	th.join();
-	auto start = chrono::steady_clock::now();
-	solve();	
-	auto end = chrono::steady_clock::now();
-	auto diff = chrono::duration <double, milli> (end-start);
-	wcout << L"Search time:" 
-		<< diff.count() 
-		<< L" ms" << endl;
-	sort(results.begin(),results.end(),comp);
-	for(auto i: results)
+	while(more)
 	{
-		wcout << i << endl;
+		wcout << L"Enter the matrix (4 lines with 4 letters NO SPACES):" << endl;
+		for(int i=0; i<ROWS; i++)
+		{
+			wcin >> matrix[i];
+			if(matrix[i].length()!=4)
+			{
+				wcout << L"\aYou made an error.\nEnter the matrix again from the beginning:" << endl;
+				//i=0;
+			}
+		}
+		wcout << L"\nSearching..." << endl;
+		
+		auto start = chrono::steady_clock::now();
+		solve();	
+		auto end = chrono::steady_clock::now();
+		auto diff = chrono::duration <double, milli> (end-start);
+		wcout << L"Search time:" 
+			<< diff.count() 
+			<< L" ms" << endl;
+		sort(results.begin(),results.end(),comp);
+		results.erase( unique( results.begin(), results.end() ), results.end() );
+		for(auto i: results)
+		{
+			wcout << i << endl;
+		}
+		//system("pause");
+		wstring temp;
+		wcout << "Input another matrix?[Y/N]:";
+		wcin >> temp;
+		if(temp!=L"Y" && temp!=L"y") more=false;
 	}
-	system("pause");
 	wcout << "Exiting...";
 	return 0;
 }
